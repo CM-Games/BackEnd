c9eea9bae37299b9c239cc6786b57049193b0362
ccm1441 <ho9273@naver.com>
2020-06-15 23:16:07 +0900

    save

---
+++ Assets/_Scripts/PlanetManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetManager : MonoBehaviour
+{
+    public static PlanetManager instance;
+
+    public float gravity = -5f;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void applyGravity(Transform rock)
+    {
+        Vector3 gravityUp = (rock.position - transform.position).normalized;
+        Vector3 rockUp = rock.up;
+
+        rock.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+
+        Quaternion targetRotation = Quaternion.FromToRotation(rockUp, gravityUp) * rock.rotation;
+      //  rock.rotation = Quaternion.Slerp(rock.rotation, targetRotation, 50 * Time.deltaTime);
+    }
+}
