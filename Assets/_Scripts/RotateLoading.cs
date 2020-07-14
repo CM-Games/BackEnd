dc0004ecb4d34283394e1273ac5b7f5b43c5911e
ccm1441 <ho9273@naver.com>
2020-07-14 20:54:16 +0900

    #.02

---
+++ Assets/_Scripts/RotateLoading.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLoading : MonoBehaviour
+{
+    void Update() => transform.Rotate(new Vector3(0, 0, -100 * Time.deltaTime));
+}
--- Assets/_Scripts/ServerManager.cs
+++ Assets/_Scripts/ServerManager.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using BackEnd;
 
 public class ServerManager : MonoBehaviour
 {
+    [Header("Login & Register")]
+    public InputField ID;
+    public InputField PW;
+
+    // 비동기 회원가입 및 로그인을 구현 할때 사용할 변수
+    BackendReturnObject bro = new BackendReturnObject();
+    bool isSuccess = false;
+
     void Start()
     {
         // 초기화
@@ -15,6 +24,7 @@ public class ServerManager : MonoBehaviour
             if (Backend.IsInitialized)
             {
                 print("뒤끝 초기화 성공");
+
                 // example
                 // 버전체크 -> 업데이트
             }
@@ -26,9 +36,79 @@ public class ServerManager : MonoBehaviour
         });
     }
 
-    // Update is called once per frame
+    // 비동기 메소드는 update()문에서 SaveToken을 꼭 적용해야 합니다.
     void Update()
     {
+        #region 회원가입 및 로그인(비동기)
+        if (isSuccess)
+        {
+            // SaveToken( BackendReturnObject bro ) -> void
+            // 비동기 메소드는 update()문에서 SaveToken을 꼭 적용해야 합니다.
+            BackendReturnObject saveToken = Backend.BMember.SaveToken(bro);
 
+            if (saveToken.IsSuccess()) print("비동기 로그인 성공");
+            else Error(bro.GetErrorCode());
+
+            isSuccess = false;
+            bro.Clear();
+        }
+        #endregion
+    }
+
+    #region 회원가입 및 로그인(동기, 비동기)
+    // 동기방식 회원가입
+    public void Register()
+    {
+        BackendReturnObject BRO = Backend.BMember.CustomSignUp(ID.text, PW.text);
+
+        if (BRO.IsSuccess()) print("동기방식 회원가입 성공");
+        else Error(BRO.GetErrorCode());
+
+    }
+
+    // 동기방식 로그인
+    public void Login()
+    {
+        BackendReturnObject BRO = Backend.BMember.CustomLogin(ID.text, PW.text);
+
+        if (BRO.IsSuccess()) print("동기방식 로그인 성공");
+        else Error(BRO.GetErrorCode());
+    }
+
+    // 비동기 방식 회원가입
+    public void RegisterAsync()
+    {
+        BackendAsyncClass.BackendAsync(Backend.BMember.CustomSignUpAsync, ID.text, PW.text, (callback) =>
+        {
+            bro = callback;
+            isSuccess = true;
+        });
+    }
+
+    // 비동기 방식 로그인
+    public void LoginAsync()
+    {
+        BackendAsyncClass.BackendAsync(Backend.BMember.CustomLoginAsync, ID.text, PW.text, (callback) =>
+        {
+            bro = callback;
+            isSuccess = true;
+        });
+    }
+    #endregion
+
+    // 에러 코드 확인
+    void Error(string errorCode)
+    {
+        switch (errorCode)
+        {
+            case "DuplicatedParameterException":
+                print("중복된 사용자 아이디 입니다.");
+                break;
+            case "BadUnauthorizedException":
+                print("잘못된 사용자 아이디 혹은 비밀번호 입니다.");
+                break;
+            default:
+                break;
+        }
     }
 }
