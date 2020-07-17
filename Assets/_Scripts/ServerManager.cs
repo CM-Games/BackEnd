7c0b3eb1041952b4baf9e92fafa65688238d9943
ccm1441 <ho9273@naver.com>
2020-07-17 18:31:58 +0900

    #.04

--- Assets/_Scripts/ServerManager.cs
+++ Assets/_Scripts/ServerManager.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using BackEnd;
+using LitJson;
 
 public class ServerManager : MonoBehaviour
 {
     [Header("Login & Register")]
     public InputField ID;
     public InputField PW;
+
+    [Header("User Info")]
+    public InputField Nickname;
+    public InputField email;
+    public Text userInfo;
 
     // 비동기 회원가입 및 로그인을 구현 할때 사용할 변수
     BackendReturnObject bro = new BackendReturnObject();
@@ -47,7 +53,7 @@ public class ServerManager : MonoBehaviour
             BackendReturnObject saveToken = Backend.BMember.SaveToken(bro);
 
             if (saveToken.IsSuccess()) print("비동기 로그인 성공");
-            else Error(bro.GetErrorCode());
+            else Error(bro.GetErrorCode(), "UserFunc");
 
             isSuccess = false;
             bro.Clear();
@@ -57,13 +63,14 @@ public class ServerManager : MonoBehaviour
 
     #region 유저 관리
 
+    #region 유저 회원가입, 로그인, 로그아웃, 회원탈퇴
     // 동기방식 회원가입
     public void Register()
     {
         BackendReturnObject BRO = Backend.BMember.CustomSignUp(ID.text, PW.text);
 
         if (BRO.IsSuccess()) print("동기방식 회원가입 성공");
-        else Error(BRO.GetErrorCode());
+        else Error(BRO.GetErrorCode(), "UserFunc");
     }
 
     // 동기방식 로그인
@@ -72,7 +79,7 @@ public class ServerManager : MonoBehaviour
         BackendReturnObject BRO = Backend.BMember.CustomLogin(ID.text, PW.text);
 
         if (BRO.IsSuccess()) print("동기방식 로그인 성공");
-        else Error(BRO.GetErrorCode());
+        else Error(BRO.GetErrorCode(), "UserFunc");
     }
 
     // 비동기 방식 회원가입
@@ -94,7 +101,7 @@ public class ServerManager : MonoBehaviour
             isSuccess = true;
         });
     }
-    
+
     // 동기 방식 로그아웃
     public void LogOut()
     {
@@ -106,7 +113,8 @@ public class ServerManager : MonoBehaviour
     // 비동기 방식 로그아웃
     public void LogOutAsync()
     {
-        BackendAsyncClass.BackendAsync(Backend.BMember.Logout, (callback) => {
+        BackendAsyncClass.BackendAsync(Backend.BMember.Logout, (callback) =>
+        {
             if (callback.IsSuccess())
             {
                 ID.text = PW.text = "";
@@ -139,22 +147,115 @@ public class ServerManager : MonoBehaviour
             }
         });
     }
+    #endregion // 유저 로그인~~~
 
-    #endregion
+    #region 유저 정보 관리
+    // 동기 방식 유저 닉네임 생성
+    public void CreateUserNickname()
+    {
+        BackendReturnObject BRO = Backend.BMember.CreateNickname(Nickname.text);
+
+        if (BRO.IsSuccess()) print("동기 방식 닉네임 생성 완료");
+        else Error(BRO.GetErrorCode(), "UserNickname");
+    }
+
+    // 동기 방식 유저 닉네임 업데이트
+    public void UpdateUserNickname()
+    {
+        BackendReturnObject BRO = Backend.BMember.UpdateNickname(Nickname.text);
+
+        if (BRO.IsSuccess()) print("동기 방식 닉네임 업데이트 완료");
+        else Error(BRO.GetErrorCode(), "UserNickname");
+    }
+
+    // 비동기 방식 유저 닉네임 생성
+    public void CreateUserNicknameAsync()
+    {
+        BackendAsyncClass.BackendAsync(Backend.BMember.CreateNickname, Nickname.text, (callback) =>
+        {
+            if (callback.IsSuccess()) print("비동기 방식 닉네임 생성 완료");
+            else Error(callback.GetErrorCode(), "UserNickname");
+        });
+    }
+
+    // 비동기 방식 유저 닉네임 업데이트
+    public void UpdateUserNicknameAsync()
+    {
+        BackendAsyncClass.BackendAsync(Backend.BMember.UpdateNickname, Nickname.text, (callback) =>
+        {
+            if (callback.IsSuccess()) print("비동기 방식 닉네임 업데이트 완료");
+            else Error(callback.GetErrorCode(), "UserNickname");
+        });
+    }
+
+    // 동기 방식 유저 정보 가져오기
+    public void getUserInfo()
+    {
+        BackendReturnObject BRO = Backend.BMember.GetUserInfo();
+
+        JsonData returnJson = BRO.GetReturnValuetoJSON()["row"];
+
+        userInfo.text = "nickName : " + returnJson["nickname"] +
+                        "\ninDate : " + returnJson["inDate"].ToString() +
+                        "\nsubscriptionType : " + returnJson["subscriptionType"].ToString() +
+                        "\nemailForFindPassword : " + returnJson["emailForFindPassword"];
+    }
+
+    // 비동기 방식 유저 정보 가져오기
+    public void getUserInfoAsync()
+    {
+        BackendAsyncClass.BackendAsync(Backend.BMember.GetUserInfo, (callback) =>
+        {
+            JsonData returnJson = callback.GetReturnValuetoJSON()["row"];
+
+            userInfo.text = "nickName : " + returnJson["nickname"] +
+                            "\ninDate : " + returnJson["inDate"].ToString() +
+                            "\nsubscriptionType : " + returnJson["subscriptionType"].ToString() +
+                            "\nemailForFindPassword : " + returnJson["emailForFindPassword"];
+        });
+    }
+
+    // 동기 방식 이메일 등록
+    public void UpdateEmail()
+    {
+        BackendReturnObject BRO = Backend.BMember.UpdateCustomEmail(email.text);
+
+        if (BRO.IsSuccess()) print("동기 방식 이메일 등록 완료");        
+    }
+
+    // 비동기 방식 이메일 등록
+    public void UpdateEmailAsync()
+    {
+        BackendAsyncClass.BackendAsync(Backend.BMember.UpdateCustomEmail, email.text, callback =>
+        {
+            if (callback.IsSuccess()) print("비동기 방식 이메일 등록 완료");
+        });
+    }
+
+
+    #endregion // 유저 정보 관리
+
+    #endregion // 유저 관리
 
     // 에러 코드 확인
-    void Error(string errorCode)
+    void Error(string errorCode, string type)
     {
-        switch (errorCode)
+        if (errorCode == "DuplicatedParameterException")
         {
-            case "DuplicatedParameterException":
-                print("중복된 사용자 아이디 입니다.");
-                break;
-            case "BadUnauthorizedException":
-                print("잘못된 사용자 아이디 혹은 비밀번호 입니다.");
-                break;
-            default:
-                break;
+            if (type == "UserFunc") print("중복된 사용자 아이디 입니다.");
+            else if (type == "UserNickname") print("중복된 닉네임 입니다.");
+        }
+        else if (errorCode == "BadUnauthorizedException")
+        {
+            if (type == "UserFunc") print("잘못된 사용자 아이디 혹은 비밀번호 입니다.");
+        }
+        else if (errorCode == "UndefinedParameterException")
+        {
+            if (type == "UserNickname") print("닉네임을 다시 입력해주세요");
+        }
+        else if (errorCode == "BadParameterException")
+        {
+            if (type == "UserNickname") print("닉네임 앞/뒤 공백이 있거나 20자 이상입니다.");
         }
     }
 }
